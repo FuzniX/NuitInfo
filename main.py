99c0d638b5604a58a8fee8845dbb6b848362d4f8
FuzniX <68467915+FuzniX@users.noreply.github.com>
2024-11-24 13:13:21 +0100

    Initializing main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request
+import random
+import string
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Gestion de l'erreur 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=26004)
